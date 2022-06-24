ef3642f12eb577902c37e2ee955cd4147c2b58fa
vamsi540 <vamsibhargav4404@gmail.com>
2022-06-24 17:28:47 +0530

    commit

---
+++ add.js
@@ -0,0 +1,21 @@
+console.log("Hello World");
+
+var a = 10;
+var b = 40;
+var c = a + b;
+console.log("Value of A is:" + a);
+console.log("Value of B is:" + b);
+console.log("Value of C is:" + c);
+
+if (c == 30)
+{
+    console.log("C is 30");
+}
+else if (c < 30) 
+{
+    console.log("C is Less Than 30:" + c);
+} 
+else 
+{
+    console.log("C is Greater Than 30:" + c);
+}
